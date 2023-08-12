d3ffe163592cdbc576ebf7a49b058004e3802be1
Kada Manohar Sai <108396261+manohar2sai@users.noreply.github.com>
2023-08-12 17:01:10 +0530

    Create Array_leader.java

---
+++ Array_leader.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Array_leader {
+    public static void main(String args[])
+    {
+       /*int arr[] = {10,22,12,3,0,6};
+        int n = arr.length;
+        int max = 0;
+        Vector<Integer> ans = new Vector<Integer>();          //optimal 
+        for(int i=n-1;i>=0;i--)
+        {
+            if(max<arr[i])
+            {
+                max = arr[i];
+                ans.add(max);
+            }
+        }
+        System.out.print(ans);*/
+
+        /*boolean leader = true;
+        Vector<Integer> ans = new Vector<Integer>();
+        for(int i=0;i<n;i++)
+        {
+            leader = true;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]<arr[j])                                brute force;
+                {
+                    leader = false;
+                    break;
+                }
+            }
+            if(leader==true)
+            {
+                ans.add(arr[i]);
+            }
+        }
+        System.out.print(ans);*/
+    }
+    
+    
+}
